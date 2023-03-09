46000f4f1046b09571c953a6b4d6aaf26aa8f8b4
Soma Li <soma@koii.network>
2023-03-09 00:46:11 -0400

    update text

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -17,8 +17,8 @@ function HomepageHeader() {
         <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
-            to="/">
-            Docusaurus Tutorial - 5min ⏱️
+            to="/concepts/koii-summary/welcome">
+            Understanding Koii
           </Link>
         </div>
       </div>
