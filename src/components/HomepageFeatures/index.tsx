7eb55de87d1eff1a35e49b2c615ff91e03df41e8
Soma Li <soma@koii.network>
2023-03-09 14:26:33 -0400

    update feature

--- src/components/HomepageFeatures/index.tsx
+++ src/components/HomepageFeatures/index.tsx
@@ -11,30 +11,24 @@ export default function HomepageFeatures(): JSX.Element {
       <h1 className="text-center big-title" > Browse the Docs By Topic </h1>
 
 <FeatureList>
-  <Column title="Understanding Koii" size={3}>
+  <Column title="Understanding Koii" size={4}>
     <Feature url="/concepts/concepts/earning-koii/get-free-tokens" title="Koii Token" subtitle="Learn the Basics about Koii Token" />
     <Feature url="/concepts/bconcepts/finnie" title="Finnie - Koii wallet" subtitle="Try Finnie as your wallet"/>
     <Feature url="/concepts/koii-summary/welcome" title="Run a Node" subtitle="Run a node to earn KOII"/>
     <Feature url="/concepts/koii-summary/welcome" title="Koii Task" subtitle="Learn about Koii Task"/>
   </Column>
-  <Column title="Developer Documentation" size={3}>
+  <Column title="Developer Documentation" size={4}>
     <Feature url="/develop/koii-software-toolkit-sdk/what-is-the-koii-sdk" title="K2 - settlement layer" subtitle="Learn how to use K2"/>
     <Feature url="/develop/koii-software-toolkit-sdk/what-is-the-koii-sdk" title="Attention Mining" subtitle="Collect Proof of Real Traffic"/>
     <Feature url="/develop/koii-software-toolkit-sdk/what-is-the-koii-sdk" title="Build a Task" subtitle="Learn how to write Koii Task"/>
     <Feature url="/develop/koii-software-toolkit-sdk/what-is-the-koii-sdk" title="Gradual Consensus" subtitle="Learn and use gradual consensus"/>
   </Column>
-  <Column title="Developer Tools" size={3}>
+  <Column title="Developer Tools" size={4}>
     <Feature url="/develop/koii-software-toolkit-sdk/what-is-the-koii-sdk" title="Koii CLI" subtitle="Learn and use Koii CLI" />
     <Feature url="/develop/koii-software-toolkit-sdk/what-is-the-koii-sdk" title="Task Node CLI" subtitle="Learn and use Task Node CLI" />
     <Feature url="/develop/koii-software-toolkit-sdk/what-is-the-koii-sdk" title="Create Task CLI" subtitle="Learn and use create-task CLI" />
     <Feature url="/develop/koii-software-toolkit-sdk/what-is-the-koii-sdk" title="JS SDK" subtitle="Learn and use JS SDK"/>
     <Feature url="/develop/koii-software-toolkit-sdk/what-is-the-koii-sdk" title="Build Dapp" subtitle="Learn and use Koii-X"/>
-  </Column>
-  <Column title="Examples & Tutorials" size={3}>
-    <Feature url="/develop/relevant-contracts/ft" title="Fungible Tokens" subtitle="Learn how to use and make FT"/>
-    <Feature url="/develop/relevant-contracts/nft" title="Non-Fungible Tokens" subtitle="Enter the NFT space"/>
-    <Feature url="/develop/relevant-contracts/dao" title="Autonomous Organizations" subtitle="Understand DAOs"/>
-    <Feature url="/tutorials/indexer/near-lake-state-changes-indexer" title="Lake Indexer" subtitle="Watch the network and access all the events"/>
   </Column>
 </FeatureList>
       </div>
