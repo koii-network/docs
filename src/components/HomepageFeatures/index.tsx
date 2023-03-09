eea1a895ff8859f0a4b48c855a9d5739308beae5
Soma Li <soma@koii.network>
2023-03-09 14:12:15 -0400

    update feature

--- src/components/HomepageFeatures/index.tsx
+++ src/components/HomepageFeatures/index.tsx
@@ -18,16 +18,14 @@ export default function HomepageFeatures(): JSX.Element {
     <Feature url="/concepts/koii-summary/welcome" title="Koii Task" subtitle="Learn about Koii Task"/>
   </Column>
   <Column title="Developer Documentation" size={3}>
-    <Feature url="/develop/quickstart-guide" title="Quickstart" subtitle="Spin-up your first dApp"/>
-    <Feature url="/tutorials/welcome" title="Tutorials & Examples" subtitle="Check-out a bast library of examples"/>
-    <Feature url="/develop/contracts/introduction" title="Build a Contract" subtitle="Learn how to write smart contracts"/>
-    <Feature url="/develop/testing/introduction" title="Test the Contract" subtitle="Write unit & integration tests"/>
-    <Feature url="/develop/integrate/frontend" title="Build a Web Frontend" subtitle="Learn how to make a web dApp"/>
-    <Feature url="/tools/realtime" title="Monitor your App" subtitle="Learn how to track the Blockchain"/>
+    <Feature url="/develop/koii-software-toolkit-sdk/what-is-the-koii-sdk" title="K2 - settlement layer" subtitle="Learn how to use K2"/>
+    <Feature url="/develop/koii-software-toolkit-sdk/what-is-the-koii-sdk" title="Attention Mining" subtitle="Collect Proof of Real Traffic"/>
+    <Feature url="/develop/koii-software-toolkit-sdk/what-is-the-koii-sdk" title="Build a Task" subtitle="Learn how to write Koii Task"/>
+    <Feature url="/develop/koii-software-toolkit-sdk/what-is-the-koii-sdk" title="Gradual Consensus" subtitle="Learn and use gradual consensus"/>
   </Column>
   <Column title="Developer Tools" size={3}>
-    <Feature url="/sdk/js/introduction" title="JavaScript SDK" subtitle="Write contracts in JavaScript" />
-    <Feature url="/sdk/rust/introduction" title="Rust SDK" subtitle="Write contracts in Rust" />
+    <Feature url="/sdk/js/introduction" title="Koii CLI" subtitle="Write contracts in JavaScript" />
+    <Feature url="/sdk/rust/introduction" title="Task Node CLI" subtitle="Write contracts in Rust" />
     <Feature url="/tools/near-cli" title="NEAR CLI" subtitle="Use NEAR from the Terminal" />
     <Feature url="/tools/near-api-js/quick-reference" title="NEAR API JS" subtitle="Interact with NEAR from JS"/>
     <Feature url="/api/rpc/introduction" title="RPC API" subtitle="Interact with the NEAR RPC API"/>
