e8d6876a2490dd2f7fc8302480fec2e048d7bbd1
soma <soma@koii.network>
2023-03-17 02:06:20 -0300

    update style

--- src/components/HomepageFeatures/featurelist.js
+++ src/components/HomepageFeatures/featurelist.js
@@ -2,7 +2,7 @@ import React from "react";
 
 export function FeatureList({ children }) {
   return <>
-    <div class="container features">
+    <div class="container features top-margin">
       <div class="row">
         {children}
       </div>
