7fdc4d8053200b0d951e5b756d489ab2fa640ffb
soma <soma@koii.network>
2023-03-20 18:17:49 -0300

    add images

--- src/components/footer/social-media-links.tsx
+++ src/components/footer/social-media-links.tsx
@@ -1,16 +1,16 @@
-// import TwitterLightBlue from "@/public/images/twitter-lightblue.svg";
-// import DiscordLightBlue from "@/public/images/discord-lightblue.svg";
-// import TelegramLightBlue from "@/public/images/telegram-lightblue.svg";
-// import YoutubeLightBlue from "@/public/images/youtube-lightblue.svg";
-// import GithubLightBlue from "@/public/images/github-lightblue.svg";
-// import LinkedinLightBlue from "@/public/images/linkedin-lightblue.svg";
+import TwitterLightBlue from "@site/static/img/twitter-lightblue.svg";
+import DiscordLightBlue from "@site/static/img/discord-lightblue.svg";
+import TelegramLightBlue from "@site/static/img/telegram-lightblue.svg";
+import YoutubeLightBlue from "@site/static/img/youtube-lightblue.svg";
+import GithubLightBlue from "@site/static/img/github-lightblue.svg";
+import LinkedinLightBlue from "@site/static/img/linkedin-lightblue.svg";
 import React from "react";
 
 export const SocialMediaLinks = () => {
   return (
     <div className="flex flex-row items-center self-center justify-between gap-8 px-6 pb-6 align-bottom">
       <a href="https://twitter.com/KoiiNetwork">
-        <Image
+        <img
           src={TwitterLightBlue}
           alt="Twitter logo"
           width={32}
@@ -18,7 +18,7 @@ export const SocialMediaLinks = () => {
         />
       </a>
       <a href="https://discord.gg/koii">
-        <Image
+        <img
           src={DiscordLightBlue}
           alt="Discord logo"
           width={32}
@@ -26,7 +26,7 @@ export const SocialMediaLinks = () => {
         />
       </a>
       <a href="https://t.me/koiinetwork">
-        <Image
+        <img
           src={TelegramLightBlue}
           alt="Telegram logo"
           width={32}
@@ -34,13 +34,13 @@ export const SocialMediaLinks = () => {
         />
       </a>
       <a href="https://github.com/koii-network/">
-        <Image src={GithubLightBlue} alt="Github logo" width={32} height={32} />
+        <img src={GithubLightBlue} alt="Github logo" width={32} height={32} />
       </a>
       <a href="https://www.linkedin.com/company/open-koi/">
-        <Image src={LinkedinLightBlue} alt="linkedin" width={32} height={32} />
+        <img src={LinkedinLightBlue} alt="linkedin" width={32} height={32} />
       </a>
       <a href="https://www.youtube.com/c/KoiiNetwork">
-        <Image src={YoutubeLightBlue} alt="linkedin" width={32} height={32} />
+        <img src={YoutubeLightBlue} alt="linkedin" width={32} height={32} />
       </a>
     </div>
   );
