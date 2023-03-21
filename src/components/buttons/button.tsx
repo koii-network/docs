68cdc6870305f0bc088097b4eb54b8d1d37717b7
Fafemi Adeola <adeola5678@gmail.com>
2023-03-21 15:20:53 +0100

    feat: buttons and sections

---
+++ src/components/buttons/button.tsx
@@ -0,0 +1,13 @@
+import React, {ReactNode} from "react";
+
+type Button = {
+    children: ReactNode;
+};
+
+export const Button = ({children}: Button) => {
+  return (
+    <button className="py-3.5 px-5 w-full rounded-md border-none no-underline hover:no-underline text-koii-white font-semibold font-base tracking-wider bg-koii-purple-2">
+        {children}
+    </button>
+  );
+};
---
+++ src/components/whereToStart/whatTodo.tsx
@@ -0,0 +1,25 @@
+import React, {ReactNode} from "react";
+
+export const WhatToDo = () => {
+  return (
+   <div className="w-full px-4 lg:px-8 max-w-[1096px] mx-auto flex flex-col gap-5 lg:gap-10 items-center mb-24">
+    <p className="text-koii-purple-2 font-semibold text-[40px] leading-[48px]">What do you want to do now?</p>
+    <div className="w-full flex flex-col lg:flex-row gap-6">
+      <a href="/concepts/koii-summary/welcome" className="w-full lg:max-w-[50%] lg:h-44">
+        <img
+          src="/img/new-web.svg"
+          alt="Discord logo"
+          className="w-full"
+        />
+      </a>
+       <a href="/develop/settlement-layer/k2-tick-tock-fast-blocks" className="w-full lg:max-w-[50%] lg:h-44">
+        <img
+          src="/img/build-impossible.svg"
+          alt="Discord logo"
+          className="w-full"
+        />
+      </a>
+    </div>
+   </div>
+  );
+};
