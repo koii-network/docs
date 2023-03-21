92fbc114b8e53e252d31ab6f51d19127f036564d
soma <soma@koii.network>
2023-03-21 12:03:32 -0300

    add policy file

--- src/components/footer/policy.tsx
+++ src/components/footer/policy.tsx
@@ -4,7 +4,7 @@ import { FooterLink } from "./footer-link";
 export const Policy = () => {
   return (
     <div className="mb-3 md:mb-0">
-      <ul className="flex flex-wrap justify-center gap-2 md:gap-6">
+      <ul className="flex flex-wrap justify-center gap-2 md:gap-6 list-none">
         <FooterLink title={"Privacy Policy"} url={"/Privacy_Policy.pdf"} />
         <FooterLink title={"Terms of Service"} url={"/TOU_June_22_2021.pdf"} />
       </ul>
