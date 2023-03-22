c6ab020c4d16437c4683b7bd2c33a41fdc87a7c5
Giftea <eleojogiftuhiene@gmail.com>
2023-03-22 10:35:11 +0100

    Addded Hero section with styling

---
+++ src/components/homePageHero/media.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Media = () => {
+  return (
+    <div>
+      <img
+        src="/img/hero.svg"
+        alt="Discord logo"
+        className="w-full"
+      />
+    </div>
+  );
+};
+
+export default Media;
