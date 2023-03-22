dc249f1d7402de437860ae413ec9dfab40786f9e
Giftea <eleojogiftuhiene@gmail.com>
2023-03-22 15:13:18 +0100

    Updated media video in hero section

--- src/components/homePageHero/media.tsx
+++ src/components/homePageHero/media.tsx
@@ -1,12 +1,16 @@
 import React from "react";
+import ReactPlayer from "react-player/youtube";
 
 const Media = () => {
   return (
-    <div>
-      <img
-        src="/img/hero.svg"
-        alt="Hero Media"
+    <div className="md:w-3/5 w-full h-[400px] md:h-[525px] lg:w-1/2">
+      <ReactPlayer
+        url="https://youtu.be/p4m4qThshfg"
+        playsinline
+        controls
+        width="100%"
         className="w-full"
+        height="100%"
       />
     </div>
   );
