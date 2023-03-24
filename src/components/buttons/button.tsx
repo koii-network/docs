06b5d40fcaa33e16f157da03a0064f568c86c785
Fafemi Adeola <adeola5678@gmail.com>
2023-03-24 16:46:01 +0100

    fix: add cursor pointer

--- src/components/buttons/button.tsx
+++ src/components/buttons/button.tsx
@@ -6,7 +6,7 @@ type Button = {
 
 export const Button = ({children}: Button) => {
   return (
-    <button className="py-3.5 px-5 w-full rounded-md border-none no-underline hover:no-underline text-koii-white font-semibold font-base tracking-wider bg-koii-purple-2">
+    <button className="py-3.5 px-5 w-full rounded-md border-none no-underline hover:no-underline text-koii-white font-semibold font-base tracking-wider bg-koii-purple-2 cursor-pointer">
         {children}
     </button>
   );
