e19b549ed45463d8499855332a39c9811b7a1735
Fafemi Adeola <adeola5678@gmail.com>
2023-03-27 22:39:36 +0100

    feat: add search

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,30 +1,30 @@
 // @ts-check
 // Note: type annotations allow type checking and IDEs autocompletion
 
-const lightCodeTheme = require('prism-react-renderer/themes/github');
-const darkCodeTheme = require('prism-react-renderer/themes/dracula');
+const lightCodeTheme = require("prism-react-renderer/themes/github");
+const darkCodeTheme = require("prism-react-renderer/themes/dracula");
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'Koii Docs',
-  tagline: 'Koii is Web3, for everyone.',
-  favicon: 'img/favicon.ico',
+  title: "Koii Docs",
+  tagline: "Koii is Web3, for everyone.",
+  favicon: "img/favicon.ico",
 
   // Set the production url of your site here
-  url: 'https://docs.koii.network/',
+  url: "https://docs.koii.network/",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: "/",
 
-  onBrokenLinks: 'warn',
-  onBrokenMarkdownLinks: 'warn',
+  onBrokenLinks: "warn",
+  onBrokenMarkdownLinks: "warn",
 
   // Even if you don't use internalization, you can use this field to set useful
   // metadata like html lang. For example, if your site is Chinese, you may want
   // to replace "en" with "zh-Hans".
   i18n: {
-    defaultLocale: 'en',
-    locales: ['en'],
+    defaultLocale: "en",
+    locales: ["en"],
   },
   plugins: [
     async function myPlugin(context, options) {
@@ -38,104 +38,122 @@ const config = {
         },
       };
     },
-    ['@docusaurus/plugin-content-docs', { 
+    [
+      "@docusaurus/plugin-content-docs",
+      {
         id: "concepts",
         path: "concepts",
         routeBasePath: "concepts/",
         sidebarPath: require.resolve("./contentSidebars.js"),
-        editUrl:
-            'https://gitlab.com/koii-network/koii-docs/-/blob/main',
+        editUrl: "https://github.com/koii-network/docs/tree/main",
         showLastUpdateAuthor: true,
         showLastUpdateTime: true,
         editCurrentVersion: true,
-        include: ['**/*.md', '**/*.mdx'],
-      }],
-    ['@docusaurus/plugin-content-docs', { 
+        include: ["**/*.md", "**/*.mdx"],
+      },
+    ],
+    [
+      "@docusaurus/plugin-content-docs",
+      {
         id: "develop",
         path: "develop",
         routeBasePath: "develop/",
         sidebarPath: require.resolve("./developSidebars.js"),
-        editUrl:
-            'https://gitlab.com/koii-network/koii-docs/-/blob/main',
+        editUrl: "https://github.com/koii-network/docs/tree/main",
         showLastUpdateAuthor: true,
         showLastUpdateTime: true,
         editCurrentVersion: true,
-        include: ['**/*.md', '**/*.mdx'],
-      }],
-      
+        include: ["**/*.md", "**/*.mdx"],
+      },
+    ],
   ],
   presets: [
     [
-      '@docusaurus/preset-classic',
+      "@docusaurus/preset-classic",
       /** @type {import('@docusaurus/preset-classic').Options} */
       ({
         docs: {
-          sidebarPath: require.resolve('./sidebars.js'),
+          sidebarPath: require.resolve("./sidebars.js"),
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
-          editUrl:
-            'https://gitlab.com/koii-network/koii-docs',
+          editUrl: "https://gitlab.com/koii-network/koii-docs",
         },
         blog: {
           showReadingTime: true,
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
-          editUrl:
-            'https://gitlab.com/koii-network/koii-docs',
+          editUrl: "https://gitlab.com/koii-network/koii-docs",
         },
         theme: {
-          customCss: require.resolve('./src/css/custom.css'),
+          customCss: require.resolve("./src/css/custom.css"),
         },
         gtag: {
-          trackingID: 'G-1MTMLP9766',
+          trackingID: "G-1MTMLP9766",
           anonymizeIP: true,
         },
       }),
     ],
   ],
   themeConfig:
-  
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
       colorMode: {
-        defaultMode: 'light',
+        defaultMode: "light",
         disableSwitch: true,
         respectPrefersColorScheme: false,
       },
       // Replace with your project's social card
       // image: 'img/thumbnail.png',
       metadata: [
-        {property: 'og:url', content: 'https://docs.koii.network/'},
-        {property: 'og:image', content: 'https://www.koii.network/static/thumbnail.png'},
-        {property: 'og:description', content: 'Koii is Web3, for everyone.'},
-        {property: 'twitter:card', content: 'summary_large_image'},
-        {property: 'twitter:description', content: 'Koii is Web3, for everyone.'},
-        {property: 'twitter:image', content: 'https://www.koii.network/static/thumbnail.png'},
+        { property: "og:url", content: "https://docs.koii.network/" },
+        {
+          property: "og:image",
+          content: "https://www.koii.network/static/thumbnail.png",
+        },
+        { property: "og:description", content: "Koii is Web3, for everyone." },
+        { property: "twitter:card", content: "summary_large_image" },
+        {
+          property: "twitter:description",
+          content: "Koii is Web3, for everyone.",
+        },
+        {
+          property: "twitter:image",
+          content: "https://www.koii.network/static/thumbnail.png",
+        },
       ],
       navbar: {
-        title: 'Koii Docs',
+        title: "Koii Docs",
         logo: {
-          alt: 'My Site Logo',
-          src: 'img/logo.svg',
+          alt: "My Site Logo",
+          src: "img/logo.svg",
         },
         items: [
           {
-            label: '📖 Learn',
-            to: '/concepts/koii-summary/welcome',
-            activeBasePath: 'concepts',
-            position: 'left',
+            label: "📖 Learn",
+            to: "/concepts/koii-summary/welcome",
+            activeBasePath: "concepts",
+            position: "left",
             className: "header-text",
           },
-           {
-            label: '💻 Build',
-            to: '/develop/settlement-layer/k2-tick-tock-fast-blocks',
-            activeBasePath: 'develop',
-            position: 'left',
+          {
+            label: "💻 Build",
+            to: "/develop/settlement-layer/k2-tick-tock-fast-blocks",
+            activeBasePath: "develop",
+            position: "left",
             className: "header-text",
           },
         ],
       },
       footer: {},
+      algolia: {
+        // The application ID provided by Algolia
+        appId: "29G9TFK9ME",
+        // Public API key: it is safe to commit it
+        apiKey: "0afa9bf81ac9b664f951bdf2f042b274",
+        indexName: "dev_koii",
+        // Optional: see doc section below
+        // contextualSearch: true,
+      },
       prism: {
         theme: lightCodeTheme,
         darkTheme: darkCodeTheme,
