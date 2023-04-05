82ddc5d659809dbcacc8275d8cc4269dc4b48a7b
Giftea <eleojogiftuhiene@gmail.com>
2023-04-05 18:01:29 +0100

    Fix: responsive issue for exploreTopics section

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,7 @@ module.exports = {
     preflight: false, // disable Tailwind's reset
   },
   content: ["./src/**/*.{js,jsx,ts,tsx}", "../docs/**/*.mdx"], // my markdown stuff is in ../docs, not /src
-  darkMode: ['class', '[data-theme="dark"]'], // hooks into docusaurus' dark mode settigns
+  darkMode: ["class", '[data-theme="dark"]'], // hooks into docusaurus' dark mode settigns
   theme: {
     extend: {
       colors: {
@@ -12,9 +12,26 @@ module.exports = {
         "koii-purple-2": "#4D3D8D",
         "koii-white": "#FCFCFC",
         "koii-blue-1": "#5ED9D1",
-        "graphite": "#4A4A4A"
-      }
+        graphite: "#4A4A4A",
+      },
+    },
+    screens: {
+      "laptop-sm": "998px",
+      'sm': '640px',
+      // => @media (min-width: 640px) { ... }
+
+      'md': '768px',
+      // => @media (min-width: 768px) { ... }
+
+      'lg': '1024px',
+      // => @media (min-width: 1024px) { ... }
+
+      'xl': '1280px',
+      // => @media (min-width: 1280px) { ... }
+
+      '2xl': '1536px',
+      // => @media (min-width: 1536px) { ... }
     },
   },
   plugins: [],
-}
+};
