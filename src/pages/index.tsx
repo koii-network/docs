935c31b07ed9c2793b3766d77e8d0872b4ef67b6
gift13 <gift@koii.network>
2023-05-13 00:54:13 +0100

    Updated links and redirects

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,7 +3,6 @@ import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
 import Layout from "@theme/Layout";
 import { ExploreTopics } from "@site/src/components/exploreTopics";
 import { Hero } from "@site/src/components/homePageHero";
-import { WhereToStart } from "../components/whereToStart";
 import { Discord } from "../components/discord";
 import { Cta } from "../components/cta";
 import HomeCard from "../components/homeCardSection";
@@ -28,7 +27,6 @@ export default function Home(): JSX.Element {
       <Cta />
       <HomeCard />
       <ExploreTopics />
-      <WhereToStart />
       <Discord />
     </Layout>
   );
