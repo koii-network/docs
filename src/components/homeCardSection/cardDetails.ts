8770511c46ac3dfd4583177ee73f61a5cc8b755c
gift13 <gift@koii.network>
2023-05-15 20:44:46 +0100

    Added thumbnails & glossary

--- src/components/homeCardSection/cardDetails.ts
+++ src/components/homeCardSection/cardDetails.ts
@@ -1,6 +1,7 @@
 export type CardItem = {
   title?: string;
   link?: string;
+  image?: string;
 };
 
 export type Cards = {
@@ -15,10 +16,26 @@ export const cardDetails: Cards[] = [
     description:
       "Learn how to harness the power of thousands of Koii Nodes to supercharge your DApp",
     items: [
-      { title: "Auth & Security", link: "/develop/koii-task-101/what-are-tasks/key-components/auth-and-security" },
-      { title: "Database Sharding", link: "/develop/koii-task-101/what-are-tasks/key-components/database-sharding" },
-      { title: "Node to Node Sync", link: "/develop/koii-task-101/what-are-tasks/key-components/node-to-node-sync" },
-      { title: "Rest API", link: "/develop/koii-task-101/what-are-tasks/key-components/rest-api" },
+      {
+        title: "Auth & Security",
+        link: "/develop/koii-task-101/what-are-tasks/key-components/auth-and-security",
+        image: "/img/AUTH & SECURITY.svg",
+      },
+      {
+        title: "Database Sharding",
+        link: "/develop/koii-task-101/what-are-tasks/key-components/database-sharding",
+        image: 'img/DATABASE SHARDING.svg'
+      },
+      {
+        title: "Node to Node Sync",
+        link: "/develop/koii-task-101/what-are-tasks/key-components/node-to-node-sync",
+        image: 'img/NODE TO NODE SYNC.svg'
+      },
+      {
+        title: "Rest API",
+        link: "/develop/koii-task-101/what-are-tasks/key-components/rest-api",
+        image: 'img/REST API.svg'
+      },
     ],
   },
 
@@ -26,10 +43,26 @@ export const cardDetails: Cards[] = [
     title: "Tutorials",
     description: "Learn by a journey through our prebuilt Koii Tasks",
     items: [
-      { title: "The Task Template", link: "/develop/write-a-koii-task/task-development-guide/k2-task-template/" },
-      { title: "Google Doodle", link: "/develop/task-tutorials/google-doodle-task/" },
-      { title: "Koii Linktree", link: "/develop/task-tutorials/linktree-task/intro" },
-      { title: "Web Crawlers", link: "/develop/task-tutorials/linktree-task/intro" },
+      {
+        title: "The Task Template",
+        link: "/develop/write-a-koii-task/task-development-guide/k2-task-template/",
+        image: 'img/TEH TASK TEMPLATE.svg'
+      },
+      {
+        title: "Google Doodle",
+        link: "/develop/task-tutorials/google-doodle-task/",
+        image: 'img/GOOGLE DOODLE (1).svg'
+      },
+      {
+        title: "Koii Linktree",
+        link: "/develop/task-tutorials/linktree-task/intro",
+        image: 'img/KOII LINKTREE.svg'
+      },
+      {
+        title: "Web Crawlers",
+        link: "/develop/task-tutorials/linktree-task/intro",
+        image: 'img/WEB CRAWLERS.svg'
+      },
     ],
   },
 
@@ -37,8 +70,14 @@ export const cardDetails: Cards[] = [
     title: "Learn",
     description: "Koii is complicated - Get the basics.",
     items: [
-      { title: "Tokenomics", link: "/concepts/the-koii-token/network-economics" },
-      { title: "Gradual Consensus", link: "/develop/koii-task-101/what-are-tasks/gradual-consensus" },
+      {
+        title: "Tokenomics",
+        link: "/concepts/the-koii-token/network-economics",
+      },
+      {
+        title: "Gradual Consensus",
+        link: "/develop/koii-task-101/what-are-tasks/gradual-consensus",
+      },
       { title: "Community", link: "/concepts/community/community-forums" },
       { title: "Glossary", link: "/concepts/glossary" },
     ],
