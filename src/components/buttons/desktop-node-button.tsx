634c2b2d926e887ea62953197427dbc00d5f4f1d
gift13 <gift@koii.network>
2023-06-07 20:29:11 +0100

    Updated Desktop node tut to have redirect button

---
+++ src/components/buttons/desktop-node-button.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const DesktopNodeButton = () => {
+  return (
+    <a href="https://www.koii.network/node" target="_blank" className="cursor-pointer">
+      <img src="/img/Run a Task Node (1).svg" />
+    </a>
+  );
+};
+
+export default DesktopNodeButton;
