0eade4a85384f5976f08b72daf4496e3df351dcb
gift13 <gift@koii.network>
2023-06-14 10:52:54 +0100

    Updated home page url

--- src/components/cta/cta.tsx
+++ src/components/cta/cta.tsx
@@ -9,7 +9,7 @@ export const Cta = () => {
     <div className='w-full px-4 md:px-8 max-w-[1300px] mx-auto flex flex-col gap-5 lg:gap-10 items-center mb-8'>
       <div className='w-full flex flex-col md:flex-row gap-6'>
         <a
-          href='/develop/koii-task-101/what-are-tasks/'
+          href='/develop/build-on-koii'
           className={'w-full lg:max-w-[50%] bg-white no-underline cursor-pointer border-x-[1px] border-b-[1px] border-solid border-t-0 border-inherit rounded-b-[10px]' + " " + styles.card}
           style={{
             textDecoration: "no-underline",
