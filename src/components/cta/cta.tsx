83f1cefbebe180228bf45cb005bb659155aa3bdd
al_koii <al@koii.network>
2023-07-03 02:36:30 -0300

    updating homepage links

--- src/components/cta/cta.tsx
+++ src/components/cta/cta.tsx
@@ -9,7 +9,7 @@ export const Cta = () => {
     <div className='w-full px-4 md:px-8 max-w-[1300px] mx-auto flex flex-col gap-5 lg:gap-10 items-center mb-8'>
       <div className='w-full flex flex-col md:flex-row gap-6'>
         <a
-          href='/develop/koii-task-101/what-are-tasks/'
+          href='/quickstart/hello-world/intro'
           className={'w-full lg:max-w-[50%] bg-white no-underline cursor-pointer border-x-[1px] border-b-[1px] border-solid border-t-0 border-inherit rounded-b-[10px]' + " " + styles.card}
           style={{
             textDecoration: "no-underline",
@@ -24,11 +24,11 @@ export const Cta = () => {
             <p className='no-underline hover:no-underline w-full flex items-start font-semibold text-2xl m-0'>
               Build on Koii Nodes
             </p>
-            <p className="m-0 text-base">Deploy your dApp on 40,000+ Koii nodes</p>
+            <p className="m-0 text-base">Deploy a hello-world app to our Community Cloud</p>
           </button>
         </a>
         <a
-          href='/run-a-node/introduction/types-of-nodes'
+          href='https://koii.network/node'
           className={'w-full lg:max-w-[50%] bg-white no-underline cursor-pointer border-x-[1px] border-b-[1px] border-solid border-t-0 border-inherit rounded-b-[10px]' + " " + styles.card}
           style={{
             textDecoration: "no-underline",
@@ -38,9 +38,9 @@ export const Cta = () => {
           <img src={"/img/COLOR- Run a Task Node.svg"} className='w-full rounded-t-[10px]' role='img' />
           <button className='p-4 bg-transparent w-full flex-col text-koii-purple-2 items-start text-start tracking-wider justify-start hover:no-underline border-none no-underline cursor-pointer'>
             <p className='no-underline hover:no-underline w-full flex items-start font-semibold text-2xl m-0 '>
-              Run a Node
+              Join the Digital Gig Economy
             </p>
-            <p className="m-0 text-base ">Rent your device to earn $KOII</p>
+            <p className="m-0 text-base ">Run automated jobs on your computer for passive income</p>
           </button>
         </a>
       </div>
