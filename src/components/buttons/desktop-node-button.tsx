46418f4f716f7bac52499fc1b94737c3501585d0
al_koii <al@koii.network>
2023-07-03 05:42:11 -0300

    updating top four landing pages and expanding tutorial options

--- src/components/buttons/desktop-node-button.tsx
+++ src/components/buttons/desktop-node-button.tsx
@@ -3,8 +3,9 @@ import React from "react";
 const DesktopNodeButton = () => {
   return (
     <a href="https://www.koii.network/node" target="_blank" className="cursor-pointer">
-      <img src="/img/Run a Task Node (1).svg" />
+      <img className="hoverAnimation" src="/img/Run a Task Node (1).svg" />
     </a>
+    
   );
 };
 
