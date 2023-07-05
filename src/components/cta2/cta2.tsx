f761dad6a86328d67d7226202e78198d4fde70d7
gift13 <gift@koii.network>
2023-07-05 17:50:32 +0100

    updated graphics

--- src/components/cta2/cta2.tsx
+++ src/components/cta2/cta2.tsx
@@ -6,72 +6,90 @@ export const Cta2 = () => {
     window.gtag("event", "click_run_node");
   };
   return (
-    <div className="w-full px-4 md:px-8 max-w-[1300px] mx-auto flex flex-col gap-5 lg:gap-10 items-center mb-8">
+    <div className="w-full px-4 md:px-8 max-w-[1300px] mx-auto flex flex-col gap-5 lg:gap-10 items-center mb-8 mt-5">
       <div className="w-full flex flex-col md:flex-row gap-6">
-        <div
+        <a
           className={
-            "w-full lg:max-w-[50%] bg-white no-underline cursor-pointer rounded-b-[10px]" +
+            "w-full lg:max-w-[50%] bg-[transparent] no-underline cursor-pointer h-full" +
             " " +
             styles.card
           }
+          href="https://koii.network/node"
+          style={{
+            textDecoration: "no-underline",
+          }}
+          onClick={handleClick}
         >
-          <a
-            href="https://koii.network/node"
-            style={{
-              textDecoration: "no-underline",
-            }}
-            onClick={handleClick}
-          >
+          <div className={styles.img}>
+            {" "}
             <img
-              src={"/img/Developer Guide.svg"}
+              src={"img/Group 3.svg"}
               className={"w-full rounded-t-[10px]"}
               role="img"
             />
-            <button
+            <img
+              src={"img/Group 6.svg"}
+              className={styles.img2}
+              role="img"
+            />
+          </div>
+          <button
+            className={
+              "p-4 bg-transparent w-full flex-col text-koii-purple-2 items-start text-start tracking-wider justify-start hover:no-underline border-none no-underline cursor-pointer" +
+              " " +
+              styles.cardButton
+            }
+          >
+            <p
               className={
-                "p-4 bg-transparent w-full flex-col text-koii-purple-2 items-start text-start tracking-wider justify-start hover:no-underline border-none no-underline cursor-pointer" +
+                "no-underline hover:no-underline w-full flex items-start font-semibold m-0 lg:text-2xl text-lg pt-5 pb-3" +
                 " " +
-                styles.cardButton
+                styles.cardText
               }
             >
-              <p className="no-underline hover:no-underline w-full flex items-start font-semibold text-2xl m-0 ">
-                Get the quick-start developer guide.
-              </p>
-            </button>
-          </a>
-        </div>
-        <div
+              Get the quick-start developer guide.
+            </p>
+          </button>
+        </a>
+        <a
           className={
-            "w-full lg:max-w-[50%] bg-white no-underline cursor-pointer rounded-b-[10px]" +
+            "w-full lg:max-w-[50%] bg-white no-underline cursor-pointer h-full" +
             " " +
             styles.card
           }
+          href="https://koii.network/node"
+          style={{
+            textDecoration: "no-underline",
+          }}
+          onClick={handleClick}
         >
-          <a
-            href="https://koii.network/node"
-            style={{
-              textDecoration: "no-underline",
-            }}
-            onClick={handleClick}
-          >
+          <div className={styles.img}>
+            {" "}
             <img
-              src={"/img/Node tutorial no bkg.svg"}
-              className="w-full rounded-t-[10px]"
+              src={"img/Group 5.svg"}
+              className={"w-full rounded-t-[10px]"}
               role="img"
             />
-            <button
+            <img src={"img/Group 7.svg"} className={styles.img3} role="img" />
+          </div>
+          <button
+            className={
+              "p-4 bg-transparent w-full flex-col text-koii-purple-2 items-start text-start tracking-wider justify-start hover:no-underline border-none no-underline cursor-pointer" +
+              " " +
+              styles.cardButton
+            }
+          >
+            <p
               className={
-                "p-4 bg-transparent w-full flex-col text-koii-purple-2 items-start text-start tracking-wider justify-start hover:no-underline border-none no-underline cursor-pointer" +
+                "no-underline hover:no-underline w-full flex items-start font-semibold m-0 lg:text-2xl md:text-base text-lg pt-5 pb-3" +
                 " " +
-                styles.cardButton
+                styles.cardText
               }
             >
-              <p className="no-underline hover:no-underline w-full flex items-start font-semibold text-2xl m-0 ">
-                This node tutorial makes it easy to join.
-              </p>
-            </button>
-          </a>
-        </div>
+              This node tutorial makes it easy to join.
+            </p>
+          </button>
+        </a>
       </div>
     </div>
   );
