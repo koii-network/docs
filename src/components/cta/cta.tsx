134bc8a3fb13bf80e42cdd71ffe1a61d47e68daf
yigitdincsoy <w0471994@campus.nscc.ca>
2023-07-27 17:55:53 -0300

    Edited the main section

--- src/components/cta/cta.tsx
+++ src/components/cta/cta.tsx
@@ -3,44 +3,60 @@ import styles from "./cta.module.css";
 
 export const Cta = () => {
   const handleClick = () => {
-    window.gtag('event', 'click_run_node')
-  }
+    window.gtag("event", "click_run_node");
+  };
   return (
-    <div className='w-full px-4 md:px-8 max-w-[1300px] mx-auto flex flex-col gap-5 lg:gap-10 items-center mb-8'>
-      <div className='w-full flex flex-col md:flex-row gap-6'>
+    <div className="w-full px-4 md:px-8 max-w-[1300px] mx-auto flex flex-col gap-5 lg:gap-10 items-center mb-8">
+      <div className="w-full flex flex-col md:flex-row gap-6">
         <a
-          href='/quickstart/hello-world/introduction'
-          className={'w-full lg:max-w-[50%] bg-white no-underline cursor-pointer border-x-[1px] border-b-[1px] border-solid border-t-0 border-inherit rounded-b-[10px]' + " " + styles.card}
+          href="/quickstart/hello-world/introduction"
+          className={
+            "w-full lg:max-w-[50%] bg-white no-underline cursor-pointer border-x-[1px] border-b-[1px] border-solid border-t-0 border-inherit rounded-b-[10px]" +
+            " " +
+            styles.card
+          }
           style={{
             textDecoration: "no-underline",
           }}
         >
           <img
             src={"/img/COLOR- Built the impossible.svg"}
-            className='w-full rounded-t-[10px]'
-            role='img'
+            className="w-full rounded-t-[10px]"
+            role="img"
           />
-          <button className='p-4 bg-transparent w-full flex-col text-koii-purple-2 items-start text-start tracking-wider justify-start hover:no-underline no-underline cursor-pointer border-none'>
-            <p className='no-underline hover:no-underline w-full flex items-start font-semibold text-2xl m-0'>
-              Build on Koii Nodes
+          <button className="p-4 bg-transparent w-full flex-col text-koii-purple-2 items-start text-start tracking-wider justify-start hover:no-underline no-underline cursor-pointer border-none">
+            <p className="no-underline hover:no-underline w-full flex items-start font-semibold text-2xl m-0">
+              For Developers
             </p>
-            <p className="m-0 text-base">Deploy a hello-world app to our Community Cloud</p>
+            <p className="m-0 text-base">
+              Deploy your first Web3 Application in 5 Minutes
+            </p>
           </button>
         </a>
         <a
-          href='https://koii.network/node'
-          className={'w-full lg:max-w-[50%] bg-white no-underline cursor-pointer border-x-[1px] border-b-[1px] border-solid border-t-0 border-inherit rounded-b-[10px]' + " " + styles.card}
+          href="https://koii.network/node"
+          className={
+            "w-full lg:max-w-[50%] bg-white no-underline cursor-pointer border-x-[1px] border-b-[1px] border-solid border-t-0 border-inherit rounded-b-[10px]" +
+            " " +
+            styles.card
+          }
           style={{
             textDecoration: "no-underline",
           }}
           onClick={handleClick}
         >
-          <img src={"/img/COLOR- Run a Task Node.svg"} className='w-full rounded-t-[10px]' role='img' />
-          <button className='p-4 bg-transparent w-full flex-col text-koii-purple-2 items-start text-start tracking-wider justify-start hover:no-underline border-none no-underline cursor-pointer'>
-            <p className='no-underline hover:no-underline w-full flex items-start font-semibold text-2xl m-0 '>
-              Join the Digital Gig Economy
+          <img
+            src={"/img/COLOR- Run a Task Node.svg"}
+            className="w-full rounded-t-[10px]"
+            role="img"
+          />
+          <button className="p-4 bg-transparent w-full flex-col text-koii-purple-2 items-start text-start tracking-wider justify-start hover:no-underline border-none no-underline cursor-pointer">
+            <p className="no-underline hover:no-underline w-full flex items-start font-semibold text-2xl m-0 ">
+              Earn Passive Income
             </p>
-            <p className="m-0 text-base ">Run automated jobs on your computer for passive income</p>
+            <p className="m-0 text-base ">
+              Run applications on your computer to earn KOII Tokens
+            </p>
           </button>
         </a>
       </div>
