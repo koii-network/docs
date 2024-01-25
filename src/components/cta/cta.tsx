3520b0119b3f82dd2ca066ed600733c06890931e
Crystal Parker <vertfromage@gmail.com>
2024-01-25 10:49:09 -0400

    fixed some errors suggested by validator.w3.org/nu

--- src/components/cta/cta.tsx
+++ src/components/cta/cta.tsx
@@ -20,9 +20,9 @@ export const Cta = () => {
           }}
         >
           <img
-            src={"/img/COLOR- Built the impossible.svg"}
+            src={"/img/COLOR-%20Built%20the%20impossible.svg"}
+            alt="Built the impossible: computer and laptop with a crane and windmill"
             className="w-full rounded-t-[10px]"
-            role="img"
           />
           <button className="p-4 bg-transparent w-full flex-col text-koii-purple-2 items-start text-start tracking-wider justify-start hover:no-underline no-underline cursor-pointer border-none">
             <p className="no-underline hover:no-underline w-full flex items-start font-semibold text-2xl m-0">
@@ -47,9 +47,8 @@ export const Cta = () => {
           onClick={handleClick}
         >
           <img
-            src={"/img/COLOR- Run a Task Node.svg"}
+            src={"/img/COLOR-%20Run%20a%20Task%20Node.svg"}
             className="w-full rounded-t-[10px]"
-            role="img"
           />
           <button className="p-4 bg-transparent w-full flex-col text-koii-purple-2 items-start text-start tracking-wider justify-start hover:no-underline border-none no-underline cursor-pointer">
             <p className="no-underline hover:no-underline w-full flex items-start font-semibold text-2xl m-0 ">
