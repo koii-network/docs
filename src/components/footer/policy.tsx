ab2826c4143e6e04de0ee60540a57c05a424b0eb
Crystal Parker <vertfromage@gmail.com>
2024-02-20 10:55:47 -0400

    updated links to evergreen pages for policy and terms

--- src/components/footer/policy.tsx
+++ src/components/footer/policy.tsx
@@ -5,11 +5,12 @@ export const Policy = () => {
   return (
     <div className="mb-3 md:mb-0">
       <div className="flex flex-wrap gap-4 md:gap-10">
-        <FooterLink title={"Terms of Service"} url={"/TOU_June_22_2021.pdf"} />
-        <FooterLink title={"Privacy Policy"} url={"/Privacy_Policy.pdf"} />
+        <FooterLink title={"Terms of Service"} url={"https://www.koii.network/terms"} target={"_blank"} />
+        <FooterLink title={"Privacy Policy"} url={"https://www.koii.network/privacy"} target={"_blank"}  />
         <FooterLink
           title={"© Koii Foundation 2023, All Rights Reserved."}
           url={"https://www.koii.network/"}
+          target={"_blank"} 
         />
       </div>
     </div>
