baa86b17fc9950601448bee748ac9e5d003fa216
yiqun Liang <herman@koii.network>
2024-04-06 17:22:03 +0100

    change to svg

--- src/components/card/cardImages.ts
+++ src/components/card/cardImages.ts
@@ -65,18 +65,18 @@ export const cardImages = {
     .default,
   carp: require(`@site/concepts/introduction/img/CARP.png`).default,
   createKoiiTask:
-    require(`@site/develop/img/Create_a_koii_task.png`)
+    require(`@site/develop/img/Create_a_koii_task.svg`)
       .default,
   koiiCLI:
-    require(`@site/develop/img/Koii_CLI.png`)
+    require(`@site/develop/img/Koii_CLI.svg`)
     .default,
   koiiJSAPI:
-    require(`@site/develop/img/Koii_JavaScript_API.png`)
+    require(`@site/develop/img/Koii_JavaScript_API.svg`)
     .default,
   Philosophy:
-    require(`@site/develop/img/Philosophy_1.png`)
+    require(`@site/develop/img/Philosophy_1.svg`)
     .default,
   tools2:
-    require(`@site/develop/img/Tools.png`)
+    require(`@site/develop/img/Tools.svg`)
     .default,
 };
