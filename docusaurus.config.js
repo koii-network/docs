01f957724b02886d6346fe33616360940cdafcc4
Laura Abro <laura@koii.network>
2024-07-23 11:28:51 -0300

    temporarily change site url to see if it fixes 404 errors

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -12,7 +12,7 @@ const config = {
   trailingSlash: false,
 
   // Set the production url of your site here
-  url: "https://www.koii.network/",
+  url: "https://homepage-new-git-test-blog-redirects-koif.vercel.app/",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: "/docs",
