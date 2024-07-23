c0449207fc4d5cd254ffca372a075ac48a0c1e45
Laura Abro <laura@koii.network>
2024-07-23 11:38:00 -0300

    try without base url

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -15,7 +15,7 @@ const config = {
   url: "https://www.koii.network/",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: "/docs",
+  baseUrl: "/",
 
   onBrokenLinks: "warn",
   onBrokenMarkdownLinks: "warn",
