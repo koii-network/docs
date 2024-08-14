773e94eb16b8b332b00fe1286bbeeae355292a89
Laura Abro <laura@koii.network>
2024-08-14 09:13:08 -0300

    fix typescript error

--- src/components/contentLink/index.tsx
+++ src/components/contentLink/index.tsx
@@ -2,50 +2,53 @@ import React from "react";
 import styles from "./content.module.css";
 import { contentImages } from "./contentImages";
 
-
 const baseUrl = process.env.BASE_URL || "";
 
 type ContentLink = {
-  title?: string;
-  link?: string;
-  iconType?: string;
-  imageLink?: string;
-  description?: string;
-  bottomText?: string;
+	title?: string;
+	link: string;
+	iconType?: string;
+	imageLink?: string;
+	description?: string;
+	bottomText?: string;
 };
 
 function ContentLinks({
-  link,
-  title,
-  iconType,
-  imageLink,
-  description,
-  bottomText,
+	link,
+	title,
+	iconType,
+	imageLink,
+	description,
+	bottomText,
 }: ContentLink) {
-  let Svg;
-  if (iconType) {
-    Svg = contentImages[iconType];
-  }
-  const target = link.includes("https") ? "_blank" : "";
-  return (
-    <>
-      <a href={`${link.includes('https') ? '' : baseUrl}${link}`} className={styles.cardContainer} target={target}>
-        {Svg && <Svg />}
-        {imageLink && (
-          <img src={imageLink} alt='logo' className={styles.imageLink} />
-        )}
-        <div className={styles.textContainer}>
-          <p className={styles.text}>{title}</p>
-          {description && (
-            <div className={styles.descriptionContainer}>{description}</div>
-          )}
-        </div>
-      </a>
-      {bottomText && (
-        <p className='mt-3 text-[#8899A8] w-full text-center'>{bottomText}</p>
-      )}
-    </>
-  );
+	let Svg;
+	if (iconType) {
+		Svg = contentImages[iconType];
+	}
+	const target = link?.startsWith("http") ? "_blank" : "";
+	return (
+		<>
+			<a
+				href={`${link?.startsWith("http") ? "" : baseUrl}${link}`}
+				className={styles.cardContainer}
+				target={target}
+			>
+				{Svg && <Svg />}
+				{imageLink && (
+					<img src={imageLink} alt="logo" className={styles.imageLink} />
+				)}
+				<div className={styles.textContainer}>
+					<p className={styles.text}>{title}</p>
+					{description && (
+						<div className={styles.descriptionContainer}>{description}</div>
+					)}
+				</div>
+			</a>
+			{bottomText && (
+				<p className="mt-3 text-[#8899A8] w-full text-center">{bottomText}</p>
+			)}
+		</>
+	);
 }
 
 export default ContentLinks;
