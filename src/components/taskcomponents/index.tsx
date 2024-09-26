4a6a861bbda5fa8dae3f9a4ad96b8980211dbc98
Laura Abro <laura@koii.network>
2024-09-26 10:13:25 -0300

    update to show correct token symbol

--- src/components/taskcomponents/index.tsx
+++ src/components/taskcomponents/index.tsx
@@ -1,47 +1,54 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
-
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 interface Task {
-  taskID: string;
-  taskName: string;
-  tokenType: ['koii', 'kpl'];
-  minStake: number;
+	taskID: string;
+	taskName: string;
+	tokenType: ["koii", "kpl"];
+	minStake: number;
+	currency: string;
 }
 
-
 const TasksComponent = () => {
-  const [tasks, setTasks] = useState<Task[]>([]);
-  const [isLoading, setIsLoading] = useState<boolean>(true);
+	const [tasks, setTasks] = useState<Task[]>([]);
+	const [isLoading, setIsLoading] = useState<boolean>(true);
 
-  useEffect(() => {
-    axios.get<{tasks: Task[]}>('https://desktop-node.api.koii.network/get-whitelisted-tasks').then((response) => {
-      setTasks(response.data.tasks);
-    }).finally(() =>
-    setIsLoading(false)
-    );
-  }, []);
+	useEffect(() => {
+		axios
+			.get<{ tasks: Task[] }>(
+				"https://desktop-node.api.koii.network/get-whitelisted-tasks"
+			)
+			.then((response) => {
+				setTasks(response.data.tasks);
+			})
+			.finally(() => setIsLoading(false));
+	}, []);
 
-  if (isLoading)  {
-    return <p>Loading...</p>
-  }
+	if (isLoading) {
+		return <p>Loading...</p>;
+	}
 
-  return (
-    <div>
-      <h1>Task List</h1>
-      {tasks.length === 0 ?  (
-        <p>Error, please retry. </p>
-      ):(
-        <ul>{tasks.map((task) => (
-            <li key={task.taskID}  className="grid grid-cols-[150px_1fr] pb-8">
-              <span className="font-bold">Name:</span> <span>{task.taskName}</span>
-              <span className="font-bold">ID:</span> <span>{task.taskID}</span>
-              <span className="font-bold">Minimum Stake:</span> <span>{(task.minStake/1000000000).toLocaleString()} KOII</span>
-            </li>
-          ))}
-        </ul>)}
-    </div>
-  );
+	return (
+		<div>
+			{tasks.length === 0 ? (
+				<p>Error, please retry. </p>
+			) : (
+				<ul>
+					{tasks.map((task) => (
+						<li key={task.taskID} className="grid grid-cols-[150px_1fr] pb-8">
+							<span className="font-bold">Name:</span>{" "}
+							<span>{task.taskName}</span>
+							<span className="font-bold">ID:</span> <span>{task.taskID}</span>
+							<span className="font-bold">Minimum Stake:</span>{" "}
+							<span>
+								{(task.minStake / 1000000000).toLocaleString()} {task.currency}
+							</span>
+						</li>
+					))}
+				</ul>
+			)}
+		</div>
+	);
 };
 
 export default TasksComponent;
