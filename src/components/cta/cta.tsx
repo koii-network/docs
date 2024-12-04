8a5c4f0722888e902b1fb1b808da1f8fecf84f9a
Laura Abro <laura@koii.network>
2024-12-04 18:12:20 -0400

    change url of write a task now

--- src/components/cta/cta.tsx
+++ src/components/cta/cta.tsx
@@ -4,63 +4,64 @@ import styles from "./cta.module.css";
 import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
 
 export const Cta = () => {
-	const { siteConfig } = useDocusaurusContext();
-	const { baseUrl } = siteConfig.customFields as { baseUrl: string };
-	const handleClick = () => {
-		window.gtag("event", "click_run_node");
-	};
-	return (
-		<div className="w-full px-4 md:px-8 max-w-[1300px] mx-auto flex flex-col gap-5 lg:gap-10 items-center mb-8">
-			<div className="w-full flex flex-col md:flex-row gap-6">
+  const { siteConfig } = useDocusaurusContext();
+  const { baseUrl } = siteConfig.customFields as { baseUrl: string };
+  return (
+    <div className="w-full px-4 md:px-8 max-w-[1300px] mx-auto flex flex-col gap-5 lg:gap-10 items-center mb-8">
+      <div className="w-full flex flex-col md:flex-row gap-6">
+        <a
+          href={`${baseUrl}/develop/task-development/write-a-task`}
+          className={
+            "w-full lg:max-w-[50%] bg-white no-underline cursor-pointer border-x-[1px] border-b-[1px] border-solid border-t-0 border-inherit rounded-b-[10px]" +
+            " " +
+            styles.card
+          }
+          style={{
+            textDecoration: "none",
+          }}
+        >
+          <img
+            src={`${baseUrl}/img/write-task.svg`}
+            className="w-full rounded-t-[10px]"
+          />
+          <button className="p-4 bg-transparent w-full flex-col text-koii-purple-2 items-start text-start tracking-wider justify-start hover:no-underline border-none no-underline cursor-pointer">
+            <p className="no-underline hover:no-underline w-full flex items-start font-semibold text-2xl m-0 ">
+              Write a Task Now
+            </p>
+            <p className="m-0 text-base ">
+              Learn how to write your own dApps on Koii with our EZSandbox
+              Tutorial
+            </p>
+          </button>
+        </a>
+        <a
+          href={`${baseUrl}/develop/task-development/kpl-tokens`}
+          className={
+            "w-full lg:max-w-[50%] bg-white no-underline cursor-pointer border-x-[1px] border-b-[1px] border-solid border-t-0 border-inherit rounded-b-[10px]" +
+            " " +
+            styles.card
+          }
+          style={{
+            textDecoration: "none",
+          }}
+        >
+          <img
+            src={`${baseUrl}/img/mint-tokens.svg`}
+            alt="Built the impossible: computer and laptop with a crane and windmill"
+            className="w-full rounded-t-[10px]"
+          />
+          <button className="p-4 bg-transparent w-full flex-col text-koii-purple-2 items-start text-start tracking-wider justify-start hover:no-underline no-underline cursor-pointer border-none">
+            <p className="no-underline hover:no-underline w-full flex items-start font-semibold text-2xl m-0">
+              Mint Your Own Tokens on Koii
+            </p>
+            <p className="m-0 text-base">
+              Empower global transactions, reward engagement, and customize your
+              digital ecosystem
+            </p>
+          </button>
+        </a>
 
-				<a
-					href="https://github.com/koii-network/ezsandbox"
-					className={
-						"w-full lg:max-w-[50%] bg-white no-underline cursor-pointer border-x-[1px] border-b-[1px] border-solid border-t-0 border-inherit rounded-b-[10px]" +
-						" " +
-						styles.card
-					}
-					style={{
-						textDecoration: "none",
-					}}
-					onClick={handleClick}
-				>
-					<img
-						src={`${baseUrl}/img/write-task.svg`}
-						className="w-full rounded-t-[10px]"
-					/>
-					<button className="p-4 bg-transparent w-full flex-col text-koii-purple-2 items-start text-start tracking-wider justify-start hover:no-underline border-none no-underline cursor-pointer">
-						<p className="no-underline hover:no-underline w-full flex items-start font-semibold text-2xl m-0 ">
-							Write a Task Now
-						</p>
-						<p className="m-0 text-base ">Learn how to write your own dApps on Koii with our EZSandbox Tutorial</p>
-					</button>
-				</a>
-				<a
-					href={`${baseUrl}/develop/task-development/kpl-tokens`}
-					className={
-						"w-full lg:max-w-[50%] bg-white no-underline cursor-pointer border-x-[1px] border-b-[1px] border-solid border-t-0 border-inherit rounded-b-[10px]" +
-						" " +
-						styles.card
-					}
-					style={{
-						textDecoration: "none",
-					}}
-				>
-					<img
-						src={`${baseUrl}/img/mint-tokens.svg`}
-						alt="Built the impossible: computer and laptop with a crane and windmill"
-						className="w-full rounded-t-[10px]"
-					/>
-					<button className="p-4 bg-transparent w-full flex-col text-koii-purple-2 items-start text-start tracking-wider justify-start hover:no-underline no-underline cursor-pointer border-none">
-						<p className="no-underline hover:no-underline w-full flex items-start font-semibold text-2xl m-0">
-							Mint Your Own Tokens on Koii
-						</p>
-						<p className="m-0 text-base">Empower global transactions, reward engagement, and customize your digital ecosystem</p>
-					</button>
-				</a>
-
-				{/*         <a
+        {/*         <a
           className={
             "lg:max-w-[50%] bg-white no-underline cursor-pointer border-x-[1px] border-b-[1px] border-solid border-t-0 border-inherit rounded-b-[10px]" +
             " " +
@@ -88,7 +89,7 @@ export const Cta = () => {
             }}
           />
         </a> */}
-			</div>
-		</div>
-	);
+      </div>
+    </div>
+  );
 };
